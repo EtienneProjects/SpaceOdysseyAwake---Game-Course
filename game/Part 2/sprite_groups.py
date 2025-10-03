2966b9b21c2b898e9b68f19faf4ab0168fa45c70
Programming_GameProjects <etiennegameprojects@gmail.com>
2025-10-03 15:16:41 +0200

    Part 2
    We now have two new enemy types and one new projectile class and many more changes

---
+++ game/Part 2/sprite_groups.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+#sprite Groups
+
+enemy_group = pygame.sprite.Group()
+player_lasers = pygame.sprite.Group()
+enemy_lasers = pygame.sprite.Group()
+heavyLaser_group = pygame.sprite.Group()
+rockets_group = pygame.sprite.Group()
+asteroid_group = pygame.sprite.Group()
+enemy_beam_group = pygame.sprite.Group()
