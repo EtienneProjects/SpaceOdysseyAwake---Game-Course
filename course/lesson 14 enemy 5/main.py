7a602fd26b12a0c802608fdd21ce7d489bcd689e
Programming_GameProjects <etiennegameprojects@gmail.com>
2025-10-03 16:50:29 +0200

    new lessons added
    added 2 lesson

---
+++ course/lesson 14 enemy 5/main.py
@@ -0,0 +1,259 @@
+import pygame
+import os
+from pygame import mixer
+import random
+import config
+import shipClass
+
+
+from sprite_groups import enemy_group, player_lasers,  heavyLaser_group, rockets_group, asteroid_group, enemy_beam_group
+from asteroid import Asteroid
+from projectiles import LaserLine
+
+
+font = pygame.font.SysFont('', 25)
+
+#create player object
+player = shipClass.Ship('player', 800, 700, 1, 10)
+
+# create player laserLine
+player_beam = LaserLine(player, is_player=True)
+
+
+# drawa text for ui
+def drawText(text, font, text_col, text_x, text_y):
+    img = font.render(text, True, text_col)
+    config.game_window.blit(img, (text_x, text_y))
+    
+# healthbar 
+health_bar = shipClass.HealthBar(10, 10, player.health, player.health)
+
+# music for game
+def play_music(song_path):
+    try:
+        pygame.mixer.music.load(song_path)
+        pygame.mixer.music.set_volume(0) # main song volume
+        pygame.mixer.music.play()# play the song without looping
+        print(f"Playing music: {song_path}") # for song print and debug
+    except Exception as e:
+        print(f"Error loading music: {e}")
+        
+current_song = 'song1' # Track the current song
+is_paused = False # Track if the game music is paused or not
+song1_path = os.path.join('audio', 'Cyberpunk Gaming Energy by Infraction [No Copyright Music]  Let us Play [q-3xl6u34Ho].mp3')
+
+# Play initial song on start
+play_music(song1_path)
+
+
+
+# background y scroll
+scroll_y = 0
+
+def draw_scrolling_bg(surface, image, state, speed=3):
+    state['y'] += speed
+    height = surface.get_height()
+
+    if state['y'] >= height:
+        state['y'] = 0
+        
+    surface.blit(image, (0, state['y'] - height))
+    surface.blit(image, (0, state['y']))
+    
+
+
+
+SPAWN_EVENT = pygame.USEREVENT + 1
+SPAWN_SINGLE_EVENT = pygame.USEREVENT + 2 # for staggered spawns
+
+spawn_interval = 12000 # 12 sec (60000 for 1 min)
+pygame.time.set_timer(SPAWN_EVENT, spawn_interval)
+
+wave_count = 1
+pending_spawns = 0 # track how many enemies are left in current wave
+
+def spawn_enemy():
+    x = random.randint(80, config.SCREEN_WIDTH - 80)
+    y = -50 # spawn above screen
+    
+    enemy_type = random.choices(
+        ['enemy1', 'enemy2', 'enemy3', 'enemy4','enemy5'],
+        weights=[3, 3, 3, 2, 1],# enemy 1,2,3 is 3 times more likley to spawn
+        k=1
+    )[0]
+
+    
+    enemy = shipClass.Ship(enemy_type, x, y, 0.5, 1)
+    enemy_group.add(enemy)
+    
+    enemy.flip = random.choice([True, False])
+    
+    
+
+
+
+
+playing = True
+while playing:
+    config.frameRate.tick(config.FPS) # get time and frame rate (loop rate)
+    config.game_window.fill(config.BLACK)
+    
+    if player.health <= 0:
+        playing = False
+        print("You died, health is: ",player.health, ", with a score of:", config.score)
+    
+    draw_scrolling_bg(config.game_window, config.scale_bg, config.scroll_state, speed=3)
+    
+    
+    player.draw()
+    player.update(player)
+    
+    
+    for rocket in rockets_group:
+        rocket.update()
+        rocket.draw()
+    
+    if config.rocket:
+        player.shoot_rocket(enemy_group, rockets_group, asteroid_group)
+        
+    if random.random() < 0.005:
+        x = random.randint(50, config.SCREEN_WIDTH - 50)
+        asteroid = Asteroid(x, -50, scale=1.0, health=20)
+        asteroid_group.add(asteroid)
+        
+    for asteroid in asteroid_group:
+        asteroid.update(asteroid_group, player)
+        asteroid.draw(config.game_window)
+    
+    
+    # update and draw enemies
+    for enemy in enemy_group:
+        enemy.update_enemy(config.SCREEN_HEIGHT)
+        enemy.draw()
+        enemy.update_lasers()
+        enemy.update(player)
+        enemy.ai_shoot(player, enemy_group, asteroid_group) #ai_shooting plain laser
+        enemy.ai_shoot_heavy(player, enemy_group, asteroid_group),
+        enemy.ai_shoot_rocket(player, rockets_group, asteroid_group)
+        if enemy.character_type == "enemy4":
+            enemy.ai_shoot_laserline(player, asteroid_group=asteroid_group, laserline_group=enemy_beam_group)
+        if enemy.character_type == "enemy5":
+            enemy.ai_shoot_enemy5(player, enemy_group, asteroid_group)
+        
+        
+    # enemy laserline
+    for beam in enemy_beam_group:
+        beam.update(asteroid_group, enemy_group, player)
+        beam.draw(config.game_window)    
+    
+        
+    #lasers
+    for laser in player_lasers:
+        laser.update()
+        laser.draw()
+    #if player shoots
+    if config.shooting:
+        player.shoot_laser(
+            target_enemy_group=enemy_group,
+            asteroid_group=asteroid_group
+        )
+    player.update_lasers()
+    
+    # laserline player shooting
+    if config.laserLine_fire:# if we are shooting
+        player_beam.trigger(True)
+    else:
+        player_beam.trigger(False)
+    player_beam.update(asteroid_group, enemy_group, player)
+    player_beam.draw(config.game_window)
+    
+        
+        
+    # heavy laser
+    heavyLaser_group.draw(config.game_window)
+    heavyLaser_group.update()
+    
+    if getattr(config, "heavy_shooting", False):
+        player.shoot_heavy(
+            target_player=player,
+            target_enemy_group=enemy_group,
+            asteroid_group=asteroid_group
+        )
+        
+    
+        
+        
+    #movement 
+    player.movement(config.moving_left, config.moving_right, config.moving_up, config.moving_down)
+    
+    
+    # ___ UI ____
+    
+    health_bar.draw(player.health)
+    drawText(f'Score: {config.score}', font, config.WHITE, 10, 25)
+    
+    
+    
+    
+    
+    # music switching logic
+    if not is_paused:
+        if not pygame.mixer.music.get_busy(): # check if song is still playing
+            current_song = 'song1'
+            play_music(song1_path) 
+        
+    
+    # events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            playing = False
+            
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT: config.moving_left = True 
+            if event.key == pygame.K_RIGHT: config.moving_right = True
+            if event.key == pygame.K_UP:    config.moving_up = True
+            if event.key == pygame.K_DOWN:  config.moving_down = True
+            if event.key == pygame.K_a: config.shooting = True
+            if event.key == pygame.K_d: config.heavy_shooting = True
+            if event.key == pygame.K_s: config.rocket = True
+            if event.key == pygame.K_w: config.laserLine_fire = True
+            
+            if event.key == pygame.K_ESCAPE: playing = False
+            
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:  config.moving_left = False
+            if event.key == pygame.K_RIGHT: config.moving_right = False
+            if event.key == pygame.K_UP:    config.moving_up = False
+            if event.key == pygame.K_DOWN:  config.moving_down = False
+            if event.key == pygame.K_a: config.shooting = False
+            if event.key == pygame.K_d: config.heavy_shooting = False
+            if event.key == pygame.K_s: config.rocket = False
+            if event.key == pygame.K_w: config.laserLine_fire = False 
+            
+            
+            
+        # _______ Spawn enemy waves ________
+        if event.type == SPAWN_EVENT:# 12s
+            pending_spawns = wave_count # number to spawn this wave
+            wave_count += 1             # next wave is 1 larger
+            spawn_enemy()
+            pending_spawns -= 1         # we spawned one so less pending now
+            if pending_spawns > 0:
+                pygame.time.set_timer(SPAWN_SINGLE_EVENT, 1000) # 100ms delay between enemies
+                
+        # _____ Stagger enemy spawns _____\
+        if event.type == SPAWN_SINGLE_EVENT:
+            spawn_enemy()               # create enemy
+            pending_spawns -= 1
+            if pending_spawns <= 0:
+                pygame.time.set_timer(SPAWN_SINGLE_EVENT, 0) # stop stagger timer
+    
+    
+    
+    
+    
+    pygame.display.update()
+
+pygame.quit()
---
+++ course/lesson 15 enemy 4 laserline/projectiles.py
@@ -0,0 +1,368 @@
+import pygame
+import os, random
+
+import config
+
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, shooter, player, enemy_group, asteroid_group, damage=10, velocity=12):
+        super().__init__()
+        self.shooter = shooter
+        self.player = player
+        self.enemy_group = enemy_group
+        self.damage = damage
+        self.velocity = velocity
+        self.asteroid_group = asteroid_group
+        
+        #load the image
+        self.image = pygame.image.load("img/laser/laser.png").convert_alpha()
+        
+        #Dirtection and start Rect
+        if shooter.character_type.startswith("enemy"):
+            self.image = pygame.transform.flip(self.image, False, True) # Enemy shoots from top downs so we need to flip in y
+            self.direction = 1
+            self.rect = self.image.get_rect(midtop=shooter.rect.midbottom)
+        else:
+            self.direction = -1 # player shoots up
+            self.rect = self.image.get_rect(midbottom=shooter.rect.midtop)
+            
+            
+    #laser update method
+    def update(self):
+        
+        #move laser
+        self.rect.y += self.velocity * self.direction
+        
+        # remove if laser goes of screen in y
+        if self.rect.bottom < 0 or self.rect.top > config.SCREEN_HEIGHT:
+            self.kill()
+            return
+        
+        # Enemy shot laser: damage player
+        if self.shooter.character_type.startswith("enemy"):
+            if self.player and self.rect.colliderect(self.player.rect):
+                if hasattr(self.player, "health"):
+                    self.player.health -= self.damage
+                self.kill()
+                return
+            
+        else:
+            #if player has shot
+            for enemy in self.enemy_group:
+                if self.rect.colliderect(enemy.rect):
+                    if hasattr(enemy, "health"):
+                        enemy.health -= self.damage
+                    self.kill()
+                    return
+                
+            # asteroid hit
+            for asteroid in self.asteroid_group:
+                if self.rect.colliderect(asteroid.rect):
+                    asteroid.health -= self.damage
+                    self.kill()
+                    return
+                
+                
+    # draw laser
+    def draw(self):
+        config.game_window.blit(self.image, self.rect)
+        
+        
+        
+        
+class HeavyLaser(Laser):
+    def __init__(self, shooter, player, enemy_group, asteroid_group, damage=50, velocity=8,  image_path="img/heavyLaser/heavylaser.png", size=(18,45)):
+        
+        #initialize base laser class 
+        super().__init__(shooter, player, enemy_group, asteroid_group, damage=damage, velocity=velocity)
+        
+        #OVERRIDE DAMAGE AND SPEED FOR ENEMIES
+        if shooter.character_type.startswith("enemy"):
+            self.damage = 10 # enemy does 10% health damage or shield damage
+            self.velocity = 6
+        else:
+            self.damage = damage # keep default damage for player
+            self.velocity = 14
+            
+        #load images and scale heavy laser
+        img = pygame.image.load(image_path).convert_alpha()
+        img = pygame.transform.scale(img, size)
+        
+        if shooter.character_type.startswith("enemy"):
+            img = pygame.transform.flip(img, False, True)# enemy looks down so we flip laser
+            self.rect = img.get_rect(midtop=shooter.rect.midbottom)
+            self.direction = 1 # facing towards screen bottom
+        else:
+            self.rect = img.get_rect(midbottom=shooter.rect.midtop)
+            self.direction = -1 # facing top screen
+            
+        self.image = img
+        
+        
+        
+        
+# rocket class 
+
+class Rocket(pygame.sprite.Sprite):
+    def __init__(self, shooter, target_group, asteroid_group):
+        super().__init__()
+        self.shooter = shooter
+        self.rocket_images = []
+        self.explosion_images = []
+        #state
+        self.exploding = False
+        self.frame_index = 0
+        self.last_update = pygame.time.get_ticks()
+        self.frame_rate = 100 #ms per frame
+        
+        # References
+        self.target_group = target_group
+        self.asteroid_group = asteroid_group
+        
+        # __load rocket images __
+        for filename in sorted(os.listdir("img/rocket")):
+            img = pygame.image.load(os.path.join("img/rocket", filename)).convert_alpha()
+            img = pygame.transform.scale(img, (20, 50))# scale the rocket down smaller
+            #flip rocket for enemies
+            if shooter.character_type.startswith("enemy"):
+                img = pygame.transform.flip(img, False, True)
+            self.rocket_images.append(img)
+            
+        #load explosion frames 
+        for filename in sorted(os.listdir("img/explosion")):
+            img = pygame.image.load(os.path.join("img/explosion", filename)).convert_alpha()
+            self.explosion_images.append(img)
+            
+        #starts with rocket sprite
+        self.image = self.rocket_images[0]
+        self.rect = self.image.get_rect(center=shooter.rect.center)
+        
+        #direction enemy or player +/-
+        if shooter.character_type.startswith("enemy"):
+            self.velocity = 4 # move for down the screen
+        else:#player
+            self.velocity = -12 # move up the screen
+            
+            
+    #update method
+    def update(self):
+        if not self.exploding:
+            # Move
+            self.rect.y += self.velocity
+            
+            # ___ Animate Rocket ___
+            current_time = pygame.time.get_ticks() # track time
+            if current_time - self.last_update > self.frame_rate:
+                self.last_update = current_time
+                self.frame_index = (self.frame_index + 1) % len(self.rocket_images)
+                self.image = self.rocket_images[self.frame_index]
+                
+            # ___ Detection Area ___
+            if self.shooter.character_type.startswith("enemy"):
+                detection_area = pygame.Rect( # create rectangle
+                    self.rect.centerx - 50,
+                    self.rect.centery - 50,
+                    80, # rocket expmx area
+                    30   # rocket exp y area       
+                    
+                )
+            else:  # player rocket
+                detection_area = pygame.Rect(
+                self.rect.centerx - 50,
+                self.rect.centery - 50,
+                100,
+                40
+                )
+                
+            
+            hit_something = False
+            
+            # ___ Check for enemies or player (ignore the shooter itself)
+            for target in self.target_group:
+                if target is self.shooter:
+                    continue
+                if detection_area.colliderect(target.rect):
+                    if hasattr(target, "health"):
+                        if self.shooter.character_type.startswith("enemy"):
+                            target.health -= 50 ## from player health
+                        else:#player
+                            target.health -= 150 # from enemy health
+                    hit_something = True
+                    
+                    
+            for asteroid in list(self.asteroid_group):
+                if hasattr(asteroid, "rect") and detection_area.colliderect(asteroid.rect):
+                    asteroid.health -= 100
+                    asteroid.break_apart(self.asteroid_group, rocket_hit=True)   
+                    
+               
+                    
+                    
+            # Trigger explosion
+            if hit_something:
+                self.exploding = True # set explosion state
+                self.frame_index = 0 # start from first image
+                self.image = self.explosion_images[self.frame_index]
+                self.rect = self.image.get_rect(center=self.rect.center)
+                config.channel_5.set_volume(0.8)
+                config.channel_5.play(config.explode_fx)
+                
+            
+            # _ Kill rocket off screen _
+            if self.rect.bottom < 0 or self.rect.top > config.SCREEN_HEIGHT:
+                self.kill()# remove object instance
+                
+        else:
+            
+            # __ Explosion Animation __
+            current_time = pygame.time.get_ticks()
+            if current_time - self.last_update > self.frame_rate:
+                self.last_update = current_time
+                self.frame_index += 1
+                if self.frame_index < len(self.explosion_images):
+                    self.image = self.explosion_images[self.frame_index]
+                    self.rect = self.image.get_rect(center=self.rect.center)
+                else:
+                    self.kill()
+            
+            
+                    
+                    
+    def draw(self):
+        config.game_window.blit(self.image, self.rect)    
+                
+        
+        
+        
+        
+        
+        
+# ___ laser line class ___
+class LaserLine(pygame.sprite.Sprite):
+    def __init__(self, ship, is_player=True):
+        super().__init__()
+        self.ship = ship
+        self.is_player = is_player
+        self.segments = [] # have a list of segments, [x, y , lenght , colour]
+        self.active = False
+        self.width = 2
+        self.speed = 35 # high speed 
+        self.color_player = [(0, 255, 255), (255, 255, 255), (255, 0, 0)] # cayan, white, red
+        self.color_ai = [(255, 0, 0), (25, 25, 0), (255, 255, 0)] # red, dark yellow/red , yellow
+        self.line_rect = pygame.Rect(0, 0, 0, 0)
+        
+        
+        #Fuel system
+        self.fuel = 100
+        self.max_fuel = self.fuel # keep track of our fuel limit
+        self.last_fuel_update = pygame.time.get_ticks()# keep track of time
+        self.fuel_drain_per_sec = 20# more than recharge
+        self.fuel_recharge_per_sec = 10 # half the drain rate
+        
+        # keep track of our sound (rapid fire can cause sound issues if not tracked)
+        self.last_shot_sound = 0
+        self.shot_sound_delay = 100 # 100 ms between bursts (10 per 1 second)
+        
+        
+    # method to check if we are trigger this rapid fire laserLine
+    def trigger(self, active):
+        self.active = active and self.fuel > 0 # we must have fuel
+        
+        
+    #update method
+    def update(self, asteroid_group, enemy_group, player):
+        # track time in now
+        now = pygame.time.get_ticks()
+        delta = (now - self.last_fuel_update) / 1000
+        self.last_fuel_update = now # reset time to start tracking event(next) again
+        
+        # play fx 
+        channel_sound = config.channel_8 if self.is_player else config.channel_9
+        
+        # FUel drain/recharge
+        if self.active and self.fuel > 0: # drain fuel when active
+            self.fuel -= self.fuel_drain_per_sec * delta
+            if self.fuel > 0.01 * self.max_fuel:
+                if not channel_sound.get_busy(): # check if it is not currently playing a sound
+                    channel_sound.set_volume(0.3)
+                    channel_sound.play(config.rapid_laser_fx, loops=-1)
+            else:# if sound is still playing
+                if channel_sound.get_busy():
+                    channel_sound.stop()
+            
+            if self.fuel <= 0:
+                self.fuel = 0
+                self.active = False
+                
+        else: # recharge fuel when not active
+            self.fuel += self.fuel_recharge_per_sec * delta
+            if self.fuel > self.max_fuel: # stop extra fuel over 100%
+                self.fuel = self.max_fuel
+            if channel_sound.get_busy():
+                channel_sound.stop()
+                
+        # Generate new segment only if active
+        if self.active:
+            x = self.ship.rect.centerx
+            y = self.ship.rect.top  if self.is_player else self.ship.rect.bottom # if player we shoot from top rect upwards if ai we shoot from bottom rect downwards
+            color = random.choice(self.color_player if self.is_player else self.color_ai)
+            lenght = random.randint(30, 50)
+            self.segments.append([x, y, lenght, color])
+            
+        # Move segments
+        for seg in self.segments:
+            seg[1] -= self.speed if self.is_player else -self.speed
+            
+        # collision check per segment
+        surviving_segments = []
+        hit_player = set() # avoid multiple hits per frame
+        
+        for seg in self.segments:
+            seg_rect = pygame.Rect(seg[0]-self.width//2, seg[1], self.width, seg[2])
+            hit = False
+            
+            #asteroid collisions
+            for asteroid in asteroid_group:
+                if seg_rect.colliderect(asteroid.rect):
+                    asteroid.health -= 30
+                    hit = True
+                    break
+                
+            # Enemy collision if it is the player
+            if self.is_player:
+                for enemy in enemy_group:
+                    if seg_rect.colliderect(enemy.rect):
+                        enemy.health -= 5
+                        hit = True
+                        
+            else:#if enemy shoots 
+                # ai laser hitting the player
+                if seg_rect.colliderect(player.rect) and player not in hit_player:
+                    # only apply when not already hit
+                    player.health -= 1
+                    hit_player.add(player)
+                    hit = True
+                
+        
+        
+            # Keep segments if it does not hit anything
+            if not hit and 0 <= seg[1] <= config.SCREEN_HEIGHT: # within this height
+                surviving_segments.append(seg)
+                
+        
+        self.segments = surviving_segments
+        
+        # update collsision rect for the whole line
+        if self.segments:
+            top_y = min(seg[1] for seg in self.segments)
+            bottom_y = max(seg[1]+seg[2] for seg in self.segments)
+            self.line_rect = pygame.Rect(self.segments[0][0]-self.width//2, top_y, self.width, bottom_y-top_y)    
+        
+        else:
+            self.line_rect = pygame.Rect(0, 0, 0, 0)
+            
+    
+    def draw(self, surface):
+        for seg in self.segments:
+            pygame.draw.rect(surface, seg[3], (seg[0]-self.width//2, seg[1], self.width, seg[2]))    
+            
