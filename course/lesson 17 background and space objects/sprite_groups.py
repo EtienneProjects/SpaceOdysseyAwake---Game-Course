492067a6398fe00c37cb02dea2d4f3b89aed4e25
Programming_GameProjects <etiennegameprojects@gmail.com>
2025-10-07 16:51:06 +0200

    lesson 17 added

---
+++ course/lesson 17 background and space objects/sprite_groups.py
@@ -0,0 +1,13 @@
+import pygame
+
+
+#sprite Groups
+
+enemy_group = pygame.sprite.Group()
+player_lasers = pygame.sprite.Group()
+enemy_lasers = pygame.sprite.Group()
+heavyLaser_group = pygame.sprite.Group()
+rockets_group = pygame.sprite.Group()
+asteroid_group = pygame.sprite.Group()
+enemy_beam_group = pygame.sprite.Group()
+explosion_group = pygame.sprite.Group()
